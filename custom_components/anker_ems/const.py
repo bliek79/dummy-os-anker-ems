c3d802d2ff6265a68535243d34cdf4afe1ba4d95
Stefan <111599445+bliek79@users.noreply.github.com>
2026-08-31 18:51:57 +0200

    Bump EMS version to 0.0.1-alpha.66

--- custom_components/anker_ems/const.py
+++ custom_components/anker_ems/const.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 DOMAIN = "anker_ems"
 NAME = "Dummy OS EMS"
-VERSION = "0.0.1-alpha.65"
+VERSION = "0.0.1-alpha.66"
 
 CONF_SIMULATION_MODE = "simulation_mode"
 
@@ -49,6 +49,7 @@ DEFAULT_MARKET_PRICE_ENTITY = "sensor.stroomvoorspeller_data"
 DEFAULT_IMPORT_MARKUP_PER_KWH = 0.1288
 DEFAULT_EXPORT_MARKUP_PER_KWH = 0.1288
 
+# Forecast / source entities.
 CONF_KNOWN_PRICE_ENTITY = "known_price_entity"
 CONF_FORECAST_PRICE_ENTITY = "forecast_price_entity"
 CONF_HOME_FORECAST_ENTITY = "home_forecast_entity"
@@ -56,65 +57,31 @@ CONF_SOLAR_TODAY_ENTITY = "solar_today_entity"
 CONF_SOLAR_TOMORROW_ENTITY = "solar_tomorrow_entity"
 CONF_SOLAR_DAY3_ENTITY = "solar_day3_entity"
 
-CONF_SOFTWARE_RESERVE_PERCENT = "software_reserve_percent"
-DEFAULT_SOFTWARE_RESERVE_PERCENT = 7.0
-
-CONF_CHARGE_EFFICIENCY_PERCENT = "charge_efficiency_percent"
-CONF_DISCHARGE_EFFICIENCY_PERCENT = "discharge_efficiency_percent"
-CONF_MINIMUM_TRADE_MARGIN = "minimum_trade_margin"
-
-DEFAULT_CHARGE_EFFICIENCY_PERCENT = 92.0
-DEFAULT_DISCHARGE_EFFICIENCY_PERCENT = 92.0
-DEFAULT_MINIMUM_TRADE_MARGIN = 0.10
-
-# Alpha25: extra operational headroom above the calculated dynamic reserve.
-# This buffer is used by the observer planner before automatic execution is enabled.
-DEFAULT_AUTO_EXECUTION_BUFFER_PERCENT = 2.0
-
-# Alpha58: calculated safety-charge residue below this amount remains visible
-# in Plan72 diagnostics but is not promoted to a physical automatic action.
-# The 0.01 kWh planner epsilon is numerical, not an execution threshold.
-MIN_ACTIONABLE_SAFETY_CHARGE_KWH = 0.10
-
-DEFAULT_KNOWN_PRICE_ENTITY = "sensor.battery_control_energy_prices"
+DEFAULT_KNOWN_PRICE_ENTITY = "sensor.stroomvoorspeller_data"
 DEFAULT_FORECAST_PRICE_ENTITY = "sensor.forecast_prices_all_in_data"
 DEFAULT_HOME_FORECAST_ENTITY = "sensor.forecast_home_consumption_data"
-DEFAULT_SOLAR_TODAY_ENTITY = "sensor.solcast_pv_forecast_voorspelling_vandaag"
-DEFAULT_SOLAR_TOMORROW_ENTITY = "sensor.solcast_pv_forecast_voorspelling_morgen"
-DEFAULT_SOLAR_DAY3_ENTITY = "sensor.solcast_pv_forecast_voorspelling_dag_3"
+DEFAULT_SOLAR_TODAY_ENTITY = "sensor.solcast_pv_forecast_forecast_today"
+DEFAULT_SOLAR_TOMORROW_ENTITY = "sensor.solcast_pv_forecast_forecast_tomorrow"
+DEFAULT_SOLAR_DAY3_ENTITY = "sensor.solcast_pv_forecast_forecast_day_3"
 
+# Source monitor entities.
 CONF_MONITOR_ENERGYZERO_ENTITY = "monitor_energyzero_entity"
 CONF_MONITOR_STROOMVOORSPELLER_ENTITY = "monitor_stroomvoorspeller_entity"
 CONF_MONITOR_SOLCAST_API_ENTITY = "monitor_solcast_api_entity"
 
-DEFAULT_MONITOR_ENERGYZERO_ENTITY = "sensor.battery_control_energy_prices"
+DEFAULT_MONITOR_ENERGYZERO_ENTITY = "sensor.energyzero_today_energy_usage"
 DEFAULT_MONITOR_STROOMVOORSPELLER_ENTITY = "sensor.stroomvoorspeller_data"
-DEFAULT_MONITOR_SOLCAST_API_ENTITY = "sensor.solcast_pv_forecast_api_laatst_geraadpleegd"
+DEFAULT_MONITOR_SOLCAST_API_ENTITY = "sensor.solcast_pv_forecast_api_used"
 
-PLATFORMS = ["sensor", "binary_sensor", "select", "number", "datetime", "switch"]
-
-PLAN_SLOT_COUNT = 3
-
-DEFAULT_BATTERY_CAPACITY_KWH = 7.2
-MIN_SOC_PERCENT = 5
-MAX_SOC_PERCENT = 100
 FORECAST_HORIZON_HOURS = 72
-
-SERVICE_START_CHARGE_TEST = "start_charge_test"
-SERVICE_START_DISCHARGE_TEST = "start_discharge_test"
-SERVICE_STOP_PHYSICAL_TEST = "stop_physical_test"
-
-TEST_MIN_POWER_W = 100
-TEST_MAX_POWER_W = 500
-TEST_DEFAULT_POWER_W = 300
-TEST_MIN_DURATION_S = 10
-TEST_MAX_DURATION_S = 120
-TEST_DEFAULT_DURATION_S = 120
-
-SERVICE_EXECUTE_SELECTED_PLAN = "execute_selected_plan"
-SERVICE_STOP_EXECUTION = "stop_execution"
-
-SERVICE_SCHEDULE_PLAN = "schedule_plan"
-SERVICE_START_PLAN_NOW = "start_plan_now"
-SERVICE_CANCEL_PLAN = "cancel_plan"
-SERVICE_STOP_ALL = "stop_all"
+DEFAULT_BATTERY_CAPACITY_KWH = 7.2
+CONF_SOFTWARE_RESERVE_PERCENT = "software_reserve_percent"
+DEFAULT_SOFTWARE_RESERVE_PERCENT = 7.0
+CONF_CHARGE_EFFICIENCY_PERCENT = "charge_efficiency_percent"
+CONF_DISCHARGE_EFFICIENCY_PERCENT = "discharge_efficiency_percent"
+CONF_MINIMUM_TRADE_MARGIN = "minimum_trade_margin"
+DEFAULT_CHARGE_EFFICIENCY_PERCENT = 92.0
+DEFAULT_DISCHARGE_EFFICIENCY_PERCENT = 92.0
+DEFAULT_MINIMUM_TRADE_MARGIN = 0.10
+MIN_ACTIONABLE_SAFETY_CHARGE_KWH = 0.10
+PLAN_SLOT_COUNT = 3
